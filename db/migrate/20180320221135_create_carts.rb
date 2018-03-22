4d0bca3a2793421e4ee8a728b0c7a375ab3d0a68
Dan Actor <danltactor@gmail.com>
2018-03-22 16:36:14 -0600

    change cart migration temporarily
    Reviewers: #kuku, rmiles19
    
    Reviewed By: #kuku, rmiles19
    
    Differential Revision: http://phabricator.devpointstudios.com/D1370

--- db/migrate/20180320221135_create_carts.rb
+++ db/migrate/20180320221135_create_carts.rb
@@ -5,7 +5,6 @@ class CreateCarts < ActiveRecord::Migration[5.1]
       t.integer :items_count
       t.float :total_price
       t.belongs_to :user, foreign_key: true
-      t.has_many :product
 
       t.timestamps
     end
