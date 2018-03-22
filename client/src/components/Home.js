9593d46ef7d52e9c0c5aec78e667b6abfa2c2b97
Devin Parkinson <dparky13@icloud.com>
2018-03-22 17:29:01 -0600

    adding styling
    Summary: button fix
    
    Reviewers: #kuku, d-actor
    
    Reviewed By: #kuku, d-actor
    
    Subscribers: d-actor
    
    Differential Revision: http://phabricator.devpointstudios.com/D1375

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Header, Image, Segment, Container, Grid, Divider, Responsive } from 'semantic-ui-react';
-import MainLogo from '../images/header-logo.png';
 import Tshirt from '../images/home/Kuku.main.jpg';
 import Iphone from '../images/home/iphone-blank.jpg';
 import appIcon from '../images/home/button-appstore.png';
-import googlePlay from '../images/home/Get_it_on_Google_play.svg';
-//this is a comment
+import googlePlay from '../images/home/Android.png';
+import styled, { keyframes } from 'styled-components';
+
+
+const AppLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
+  fluid
+`
 
 class Home extends Component {
   render() {
@@ -20,11 +26,10 @@ class Home extends Component {
               mobile={16}
               tablet={8}
               >
-              <Responsive as="image" minWidth={750}>
+              <Responsive as="Image" minWidth={750}>
               <Image style={styles.iPhone} centered width="35%" src={Iphone}/>
               </Responsive>
             </Grid.Column>
-            <Divider vertical />
             <Grid.Column
               computer={8}
               mobile={16}
@@ -36,34 +41,29 @@ class Home extends Component {
           </Grid.Row>
           </Grid>
         </Container>
-          <Segment
-            centered
-            mobile={16}
-            tablet={16}
-            computer={4}>
-            <p>Download The App Today!</p>
-            <Image
-              src={appIcon}
-              as='a'
-              href='https://www.apple.com/ios/app-store/'
-              size='small'
-              target='_blank'
-              rel='noopener noreferrer'
-              centered
-              />
-            <br />
-            <br />
-            <br />
-            <Image
-              src={googlePlay}
-              as='a'
-              href='https://play.google.com/store/apps?hl=en'
-              size='small'
-              target='_blank'
-              rel='noopener noreferrer'
-              centered
-              />
-          </Segment>
+          <Divider />
+          <Container>
+              <AppLinks>
+                <Image
+                  src={appIcon}
+                  as='a'
+                  href='https://www.apple.com/ios/app-store/'
+                  size='small'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  centered
+                />
+                <Image
+                  src={googlePlay}
+                  as='a'
+                  href='https://play.google.com/store/apps?hl=en'
+                  size='small'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  centered
+                  />
+            </AppLinks>
+          </Container>
       </Segment>
     );
   }
@@ -87,6 +87,6 @@ const styles = {
     width: '160px'
   }
 
-} //small change to update
+}
 
 export default Home;
