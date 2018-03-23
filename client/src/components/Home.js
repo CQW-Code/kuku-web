b0d775cc56ad0b07cab94e7c1ebe7e9b9645cbd8
Devin Parkinson <dparky13@icloud.com>
2018-03-22 22:11:34 -0600

    phones added
    Reviewers: #kuku, bcaya
    
    Reviewed By: #kuku, bcaya
    
    Differential Revision: http://phabricator.devpointstudios.com/D1392

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Header, Image, Segment, Container, Grid, Divider, Responsive } from 'semantic-ui-react';
+import { Image, Segment, Container, Grid, Divider, Responsive } from 'semantic-ui-react';
 import Tshirt from '../images/home/Kuku.main.jpg';
-import Iphone from '../images/home/iphone-blank.jpg';
+import Iphone from '../images/home/IphoneX.png';
 import appIcon from '../images/home/button-appstore.png';
-import googlePlay from '../images/home/Android.png';
-import styled, { keyframes } from 'styled-components';
+import googlePlay from '../images/home/AndroidLink.png';
+import styled from 'styled-components';
+import Android from '../images/home/Android.png';
+import { Link } from 'react-router-dom';
 
 
 const AppLinks = styled.div`
@@ -13,12 +15,26 @@ const AppLinks = styled.div`
   fluid
 `
 
+const ButtonLink = styled.a`
+  border-radius: 4px;
+  float: right;
+  padding: 10px 30px;
+  border: 2px solid black
+  border-radius: 10px;
+  background: linear-gradient(to bottom right, #838383, black, #838383);
+  font-weight: bold;
+`
+
 class Home extends Component {
   render() {
     return (
       <Segment basic style={ styles.mainContainer}>
         <Image style={styles.image} height="560px" fluid centered src={Tshirt} />
-        <Container style={styles.middleContainer} height="400px">
+        <Container style={styles.middleContainer}>
+          <Divider />
+          <ButtonLink>
+            <Link to="/" style={{ color: 'white' }}>Test It Out For Free!</Link>
+          </ButtonLink>
         <Grid>
           <Grid.Row centered columns={3}>
             <Grid.Column
@@ -35,11 +51,31 @@ class Home extends Component {
               mobile={16}
               tablet={8}
             >
-              <h2>Test</h2>
-              <p> slave food dispenser. Why must they do that hack up furballs yet use lap as chair thinking longingly about tuna brine you have cat to be kitten me right meow but hit you unexpectedly but climb a tree. Soft kitty warm kitty little ball of furr purrrrrr find empty spot in cupboard and sleep all day milk the cow for white cat sleeps on a black shirt destroy the blinds and all of a sudden cat goes crazy. </p>
+              <h2>Try a Fun New Way To Shop.</h2>
+              <p>Why must they do that hack up furballs yet use lap as chair thinking longingly about tuna brine you have cat to be kitten me right meow but hit you unexpectedly but climb a tree. Soft kitty warm kitty little ball of furr purrrrrr find empty spot in cupboard and sleep all day milk the cow for white cat sleeps on a black shirt destroy the blinds and all of a sudden cat goes crazy. </p>
             </Grid.Column>
           </Grid.Row>
           </Grid>
+          <Grid>
+            <Grid.Row centered columns={3}>
+              <Grid.Column
+                computer={8}
+                mobile={16}
+                tablet={8}
+              >
+                <h2>Like Tinder But Every Match Is A Winner!</h2>
+                <p>Hodor hodor - hodor... Hodor hodor hodor hodor hodor. Hodor. Hodor hodor - hodor, hodor. Hodor hodor hodor! Hodor, hodor - hodor?! Hodor hodor - hodor... Hodor hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, HODOR hodor, hodor hodor... Hodor hodor hodor. Hodor, hodor, hodor. Hodor HODOR hodor, hodor HODOR hodor, hodor hodor. Hodor. Hodor. Hodor hodor; hodor hodor? </p>              </Grid.Column>
+              <Grid.Column
+                computer={8}
+                mobile={16}
+                tablet={8}
+                >
+                <Responsive as="Image" minWidth={750}>
+                  <Image style={styles.Android} centered width="35%" src={Android}/>
+                </Responsive>
+              </Grid.Column>
+            </Grid.Row>
+            </Grid>
         </Container>
           <Divider />
           <Container>
@@ -78,7 +114,7 @@ const styles = {
   },
 
   middleContainer: {
-    height:'540px',
+    height:'900px',
     background: 'white',
     width: '100%',
     justifyContent: 'center',
